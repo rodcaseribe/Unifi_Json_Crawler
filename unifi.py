c5f95221cb1a3ff570ec243e0a98566f57d1e840
rodcaseribeiro <rodolfo2804@hotmail.com>
2019-05-21 09:27:29 -0300

    Create unifi.py

---
+++ unifi.py
@@ -0,0 +1,36 @@
+import sys
+import json
+import string
+from pprint import pprint
+import re
+from json.decoder import JSONDecoder
+import os
+import requests
+
+
+data = '{"username":"admin","password":"91111000","strict":true}'
+response = requests.post('https://10.0.9.2:8443/api/login', data=data, verify=False)
+cookies = {
+}
+response = requests.get('https://10.0.9.2:8443/api/s/default/stat/device', cookies=cookies, verify=False)
+
+
+print ("<body>")
+with open('output.json') as json_file2:
+        datas = json.load(json_file2)
+        x = 0
+        while(True):
+                try:
+                        print ('<font style="color:#525244;">')
+                        print ("\n"+json.dumps(datas["data"][x]["name"]))
+                        #print (json.dumps(datas["data"][x]["vap_table"][0]["essid"])+ " " + json.dumps(datas["data"][x]["adopted"]) )
+                        a= (json.dumps(datas["data"][x]["adopted"]))
+                        if(a) == "true":     #VERFIFICACAO
+                                a = ("OK")
+                                b =(json.dumps(datas["data"][x]["vap_table"][0]["essid"]))
+                                #print (json.dumps(datas["data"][x]["vap_table"][0]["essid"])+ " "+a)
+                                print ('<font color="#673ab7">'+b+'</font></font><font color=green><b>'+a+'</b></font></font><br>')
+                        x += 1
+                except:
+                        print("</body>")
+                        break
