b22bb732adcc6de7691d433810dfcd15eb6f50ca
rodcaseribeiro <rodolfo2804@hotmail.com>
2019-05-21 09:49:22 -0300

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -6,7 +6,7 @@ echo ""
 
 echo '<html>'
 echo '<head>'
-echo '<title>UNICEP- CAMPUS 2</title>'
+echo '<title>Monitoramento 2</title>'
 echo '<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"><style>h2,p,font{font-family: 'Raleway', sans-serif;}</style>'
 echo '</head>'
 echo '<body style="background-color:white">'
